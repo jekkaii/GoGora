417fef21208293692eab09c69cdd5f8e9e915873
gabdownbad <2227003@slu.edu.ph>
2024-10-16 10:17:02 +0800

    gogora passenger log in

---
+++ passenger/script.js
@@ -0,0 +1,11 @@
+document.getElementById('show-login-form').addEventListener('click', function(e) {
+    e.preventDefault();
+    document.getElementById('register-form').style.display = 'none';
+    document.getElementById('login-form').style.display = 'block';
+});
+
+document.getElementById('show-register-form').addEventListener('click', function(e) {
+    e.preventDefault();
+    document.getElementById('login-form').style.display = 'none';
+    document.getElementById('register-form').style.display = 'block';
+});
