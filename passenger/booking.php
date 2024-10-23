963214de5396d4fd52e37db0ebe66bd07775cc8e
jekkaii <2216683@slu.edu.ph>
2024-10-23 21:33:13 +0800

    updated db, added style2, updated booking

--- passenger/booking.php
+++ passenger/booking.php
@@ -170,7 +170,7 @@ $conn->close();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>GoGora Booking</title>
-    <link rel="stylesheet" href="styles.css">
+    <link rel="stylesheet" href="style2.css">
 </head>
 <body id="book-bod">
     <!-- Header Section -->
