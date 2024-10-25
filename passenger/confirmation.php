55487557a7fea56617cf0731247bceeb4412af9d
Meree Iris Dorothy Lopez <2223448@slu.edu.ph>
2024-10-26 01:03:56 +0800

    added favicon

--- passenger/confirmation.php
+++ passenger/confirmation.php
@@ -85,6 +85,7 @@ $conn->close();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Booking Confirmation</title>
+    <link rel="icon" type="image/png" href="/assets/favicon.png"> 
     <link rel="stylesheet" href="styles.css">
 </head>
 <body id="confirm-bod">
